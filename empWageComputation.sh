bfa80f4db1c1d49222eb3a0fb797b48536f62231
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 10:39:13 +0530

    Use Case 2 - Calculating Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,13 +4,36 @@ echo "Welcome to Employee Wage Computation Program"
 
 #Use Case 1 (Check whether employee is present or not)
 
+#empCheck=$((RANDOM%2))
+#
+#IS_PRESENT=1
+#
+#if [ $empCheck -eq $IS_PRESENT ]
+#then
+#        echo "Employee is Present"
+#else
+#        echo "Employee is Absent"
+#fi
+
+
+
+
+#Use Case 2 (Calculate Daily Employee Wage)
+
 empCheck=$((RANDOM%2))
 
 IS_PRESENT=1
+EMP_WAGE_PER_HR=20
+
+empHr=0
 
 if [ $empCheck -eq $IS_PRESENT ]
 then
         echo "Employee is Present"
+       empHr=8
 else
         echo "Employee is Absent"
+       empHr=0
 fi
+
+echo "Employee's Daily Wage: "$((EMP_WAGE_PER_HR*empHr))
