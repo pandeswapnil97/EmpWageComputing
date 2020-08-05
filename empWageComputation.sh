2c746dc44956f1df790d24fcb8134b9137fa9c75
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 10:28:59 +0530

    Use Case 1 - Checking whether employee is present or not

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program"
+
+#Use Case 1 (Check whether employee is present or not)
+
+empCheck=$((RANDOM%2))
+
+IS_PRESENT=1
+
+if [ $empCheck -eq $IS_PRESENT ]
+then
+        echo "Employee is Present"
+else
+        echo "Employee is Absent"
+fi
