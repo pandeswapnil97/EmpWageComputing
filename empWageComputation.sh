13c7f6b4288b0775233e56d10fdca34d58b9c07e
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 11:51:39 +0530

    Use Case 8 - Storing Daily and Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -215,6 +215,70 @@ echo "Welcome to Employee Wage Computation Program"
 
 
 #Use Case 7 (Writing functions to get work hours)
+#
+#echo "          "
+#
+#IS_FULL_TIME=1
+#IS_PART_TIME=2
+#EMP_WAGE_PER_HR=20
+#NUMBER_OF_WORKING_DAYS=20
+#MAX_WORKING_HRS=100
+#
+#
+#totalEmpHr=0
+#totalWorkingDays=0
+#
+#declare -A daily_emp_wage
+#
+#function getEmpHrs()
+#{
+#        empCheck=$1
+#       case $empCheck in
+#                $IS_PART_TIME)
+#                        empHr=4
+#                ;;
+#                $IS_FULL_TIME)
+#                        empHr=8
+#                ;;
+#                *)
+#                        empHr=0
+#                ;;
+#        esac
+#
+#        echo $empHr
+#}
+#
+#
+#while [[ $totalEmpHr -le $MAX_WORKING_HRS && $totalWorkingDays -le $NUMBER_OF_WORKING_DAYS ]]
+#do
+#        ((totalWorkingDays++))
+#
+#        empCheck=$((RANDOM%3))
+#
+#        empHr=$( getEmpHrs $empCheck )
+#
+#       daily_emp_wage["Day"$totalWorkingDays]=$((EMP_WAGE_PER_HR*empHr))
+#
+#        totalEmpHr=$((totalEmpHr+empHr))
+#done
+#
+#for i in "${!daily_emp_wage[@]}"
+#do
+#        echo "$i:${daily_emp_wage[$i]}"
+#done
+#
+#echo "          "
+#
+#echo "Total Employee Hours of Employee in a month: "$totalEmpHr
+
+
+
+
+
+
+
+
+#Use Case 8 (Storing Daily Wage along with Total Wage)
 
 echo "          "
 
@@ -257,9 +321,11 @@ do
 
         empHr=$( getEmpHrs $empCheck )
 
-       daily_emp_wage["Day"$totalWorkingDays]=$((EMP_WAGE_PER_HR*empHr))
+        daily_emp_wage["Day"$totalWorkingDays]=$((EMP_WAGE_PER_HR*empHr))
 
         totalEmpHr=$((totalEmpHr+empHr))
+
+       totalEmpWage=$((totalEmpHr*EMP_WAGE_PER_HR))
 done
 
 for i in "${!daily_emp_wage[@]}"
@@ -270,3 +336,8 @@ done
 echo "          "
 
 echo "Total Employee Hours of Employee in a month: "$totalEmpHr
+
+echo "          "
+
+echo "Total Employee Wage of Employee in a month: "$totalEmpWage
+
