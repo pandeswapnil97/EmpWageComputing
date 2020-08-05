8bf3a9cbaf7436c097efca970ed9f64f9500acb1
pandeswapnil97 <pandeswapnil97@gmail.com>
2020-08-05 11:29:02 +0530

    Use Case 4 - Solving Using Switch Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -45,6 +45,36 @@ echo "Welcome to Employee Wage Computation Program"
 
 
 #Use Case 3 (Adding Part Time Employee and Wage)
+#
+#empCheck=$((RANDOM%3))
+#
+#IS_FULL_TIME=1
+#IS_PART_TIME=2
+#EMP_WAGE_PER_HR=20
+#
+#empHr=0
+#
+#if [ $empCheck -eq $IS_FULL_TIME ]
+#then
+#       echo "Employee is Full-Time"
+#               empHr=8
+#elif [ $empCheck -eq $IS_PART_TIME ]
+#then
+#        echo "Employee is Part_Time"
+#               empHr=4
+#else
+#       echo "Employee is Absent"
+#       empHr=0
+#fi
+#
+#echo "Employee's Daily Wage: "$((EMP_WAGE_PER_HR*empHr))
+
+
+
+
+
+
+#Use Case 4 (Solving Using Switch Case)
 
 empCheck=$((RANDOM%3))
 
@@ -54,17 +84,19 @@ EMP_WAGE_PER_HR=20
 
 empHr=0
 
-if [ $empCheck -eq $IS_FULL_TIME ]
-then
-       echo "Employee is Full-Time"
-               empHr=8
-elif [ $empCheck -eq $IS_PART_TIME ]
-then
-        echo "Employee is Part_Time"
+case $empCheck in
+       $IS_FULL_TIME)
+                echo "Employee is Full-Time"
+                empHr=8
+       ;;
+       $IS_PART_TIME)
+               echo "Employee is Part_Time"
                empHr=4
-else
-       echo "Employee is Absent"
-       empHr=0
-fi
+       ;;
+       *)
+               echo "Employee is Absent"
+               empHr=0
+       ;;
+esac
 
 echo "Employee's Daily Wage: "$((EMP_WAGE_PER_HR*empHr))
